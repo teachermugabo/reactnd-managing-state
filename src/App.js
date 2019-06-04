11bb9371cf11a4c4787935fbcc9d230fb922d5b1
Thierry Uwilingiyimana <thierry.uwi@gmail.com>
2019-06-04 02:37:02 +0000

    Working Game
    Did a lot to this game. I added a "New Game" button. It has quirks
    at the very beginning where the calculated sum may be 0+0+0=1.
    That would need to be handled so that the initial value is zero.
    I wonder how the possible solution did it. I must have fiddled with
    this exercise for a very long long time. And yet, I didn't look at
    the solution. this is what I want from my students. But how do we
    get ther? I am not sure.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
 
 class App extends Component {
   state = {
     numQuestions: 0,
     numCorrect: 0,
+    value1: 0,
+    value2: 0,
+    value3: 0,
   }
 
   onClickingTrue = () => {
-    console.log("Hi there!...clicking true");
-  	
+    console.log("Hi there!...clicking true")
+
+	if(this.state.proposedAnswer === (this.state.value1 + this.state.value2 + this.state.value3)) {
+      this.setState((state) => (
+        {numCorrect : state.numCorrect + 1}
+      ))
+    }
     this.incrementNumQuestion();
   }
 
   onClickingFalse = () => {
     console.log("Hi there!...clicking false");
+    
+    if(this.state.proposedAnswer !== (this.state.value1 + this.state.value2 + this.state.value3)) {
+      this.setState((state) => (
+      	{numCorrect : state.numCorrect + 1}
+       ))
+    }
     
     this.incrementNumQuestion();
   }
@@ -30,7 +40,19 @@ class App extends Component {
       {numQuestions : state.numQuestions + 1}))
   }
 
+  newGame = () => {
+    this.setState({
+      value1 : Math.floor(Math.random() * 100),
+      value2 : Math.floor(Math.random() * 100),
+      value3 : Math.floor(Math.random() * 100),
+  	})
+  }
+
   render() {
+    const proposedAnswer = Math.floor(Math.random() * 3) 
+		+ this.state.value1 
+		+ this.state.value2 
+		+ this.state.value3
     
     return (
       <div className="App">
@@ -41,10 +63,11 @@ class App extends Component {
         <div className="game">
           <h2>Mental Math</h2>
           <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+            <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${proposedAnswer}`}</p>
           </div>
           <button onClick={this.onClickingTrue}>True</button>
           <button onClick={this.onClickingFalse}>False</button>
+		  <button onClick={this.newGame}>New Game</button>
           <p className="text">
             Your Score: {this.state.numCorrect}/{this.state.numQuestions}
           </p>
