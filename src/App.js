c2a97306a5bad5fc03b2a1360c85354b35e5c1cf
Thierry Uwilingiyimana <thierry.uwi@gmail.com>
2019-06-04 01:15:34 +0000

    first commit -- after starting

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const value1 = Math.floor(Math.random() * 100);
+const value2 = Math.floor(Math.random() * 100);
+const value3 = Math.floor(Math.random() * 100);
+const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
+
+class App extends Component {
+  state = {
+    numQuestions: 0,
+    numcurrect: 0,
+  }
+
+  const onClickingTrue = () => {
+  };
+
+  const onClickingFalse = () = {
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="game">
+          <h2>Mental Math</h2>
+          <div className="equation">
+            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+          </div>
+          <button onClick={() => this.onClickingTrue}>True</button>
+          <button onClick={() => this.onClickingFalse}>False</button>
+          <p className="text">
+            Your Score: {numCorrect}/{numQuestions}
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
